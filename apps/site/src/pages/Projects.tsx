dccf34032fd06166ee1d53eb095c1facd705a419
Nathaniel Judd <nathaniel_judd@churchofjesuschrist.org>
2026-01-07 12:20:50 -0800

    Create beginning of Projects page

--- apps/site/src/pages/Projects.tsx
+++ apps/site/src/pages/Projects.tsx
@@ -7,20 +7,20 @@ const Projects = () => {
             <span className="text-2xl sm:text-4xl text-center block py-4 font-bold text-gray-800">Personal Projects</span>
             <ul className="list-disc px-4">
                 <li className="flex flex-col">
-                    <span className="font-bold text-gray-800 text-xl sm:text-2xl">Portfolio Page (You Are Here!):</span>
+                    <span className="font-bold text-gray-800 text-xl sm:text-2xl">Portfolio Page (You Are Here):</span>
                     <span className="text-gray-700 text-md sm:text-lg">
-                        Web application built using Rsbuild React.js, using Tailwind CSS and a Hono backend.
+                        You're interacting with a web application built using Rsbuild & React.js, using Tailwind CSS and a Hono backend.
                         <br />
-                        Hosted on AWS using S3, Lambda, Cloudfront & API Gateway
+                        Hosted on AWS using S3, Lambda, Cloudfront & API Gateway,
                         <br />
-                        Automatically deployed with Terraform IaC and Azure DevOps Pipelines
+                        automatically deployed with Terraform IaC and Azure DevOps Pipelines.
                     </span>
                     <span className="font-bold text-md sm:text-lg text-gray-700">
-                        See the code
+                        See the code&#8200;
                         <a href="https://github.com/Judd2000/personal-portfolio.git" target="_blank" className="underline">HERE</a>
-                        (GitHub)
+                        &#8200;(GitHub)
                     </span>
-                    <img src={PortfolioPageCode} className="w-full sm:w-1/2" />
+                    <img src={PortfolioPageCode} className="w-full md:w-1/2" />
                 </li>
             </ul>
         </div>
